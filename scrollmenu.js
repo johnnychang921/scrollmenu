b0144acacc3193c1dce65845f69e7c1b2a1c9e3c
johnny chang <bibby7292@gmail.com>
2018-08-27 16:35:26 +0800

    add gototop

--- scrollmenu.js
+++ scrollmenu.js
@@ -10,4 +10,21 @@
 			}
 		});
 	};
+
+	$.fn.gototop = function(){
+		var gototop = this;
+		$(window).scroll(function(){
+			var bodyheight = $(window).scrollTop();
+			if(bodyheight >= 300){
+				gototop.css('opacity', '1');
+			}else{
+				gototop.css('opacity', '0');
+			}
+			console.log(bodyheight);
+		});
+		this.click(function(){
+			jQuery('html,body').animate({ scrollTop: 0 }, 1000);
+			return false;
+		});
+	};
 })(jQuery);
